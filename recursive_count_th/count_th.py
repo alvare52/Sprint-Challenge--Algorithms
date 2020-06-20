644fb87c4338a05227426d09ae17dc3400748bb1
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-06-19 17:03:11 -0700

    ehhh

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,9 +5,16 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     # TBC
-    main_count = word.count("th")
+    # main_count = word.count("th")
 
-    if main_count != word.count("th"):
-        count_th(word)
+    # if main_count != word.count("th"):
+    #     count_th(word)
 
-    return word.count("th")
+    # return word.count("th")
+    if len(word) < 2: 
+        return 0
+    if word[0] == "t" and word[1] == "h":
+        return 1 + count_th(word[2:])
+    else:
+        return count_th(word[1:])
+
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,33 +96,126 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # if held item is LESS than item in front of it
-        # print(" --- SORT CALLED --- ")
-        # print(f"helf item: {self._item}, positon: {self._position}")
-        # print(f"COMPARE, -1 = held < next, 1 = held > next ... {self.compare_item()}")
-        # print("NOW SWAPPING")
+        print("--- BEGIN ---")
+        # # pick up first item
         # self.swap_item()
-        # print(f"held item: {self._item}, positon: {self._position}")
+        # print(f"helf item: {self._item}, positon: {self._position}")
+
+        # # turn on light
+        # self.set_light_on()
+        # # while light is on ....
+
+        # # keep moving right until you can't
+        # while self.can_move_right():
+        #     self.move_right()
+            
+        #     # if held is GREATER than in front, swap
+        #     if self.compare_item() == 1:
+        #         self.set_light_on
+        #         self.swap_item()
+
+        # # END of list, can't move right anymore
+        # print("END of list")
+        # print(f"helf item: {self._item}, positon: {self._position}")
+
+        # # move all the way to the left until you can't
+        # while self.can_move_left():
+        #     self.move_left()
+
+        # # START of list, can't move left anymore
+        # print("START of list")
+        # print(f"helf item: {self._item}, positon: {self._position}")
+
+        # # Held item should be smallest item in list, so put back at postition 0
+        # self.swap_item()
+        # print(f"helf item: {self._item}, positon: {self._position}")
+
+        # -------
+
+        # if self.can_move_right:
+        #     self.move_right
+        #     # pick up item to right of postition 0, to start all over from next index
+        #     self.swap_item
+
+        # start
+        # pick up first item
         self.swap_item()
         print(f"helf item: {self._item}, positon: {self._position}")
+
+        # turn on light
+        self.set_light_on()
+        
+        # while light is on ....
+
+       
+
+
+        # search for smallest
         while self.can_move_right():
             self.move_right()
+            
+            # if held is GREATER than in front, swap
             if self.compare_item() == 1:
+                self.set_light_on
                 self.swap_item()
-        print("end of list")
-        print(f"helf item: {self._item}, positon: {self._position}")
+
+        # ------
+
+        # move all the way to the left until you can't
         while self.can_move_left():
             self.move_left()
-        print("start of list")
-        print(f"helf item: {self._item}, positon: {self._position}")
-        self.swap_item()
+
+        #  keep doing until light is off
+        while self.light_is_on():
+            
+            self.set_light_off()
+
+            # until you get to end, we have lowest as our held temp
+            while self.can_move_right():
+                self.move_right() # i += 0
+                self.set_light_off()
+
+                # go right, swap, then go back
+                if self.can_move_right():
+                    self.move_right()
+                    self.swap_item()
+                    self.move_left()
+
+                # if held (old next) is LESS than front
+                if self.compare_item() == -1:
+                    self.swap_item()
+                    self.move_right()
+                    self.swap_item()
+                    self.set_light_on()
+
+
+        # ------ Put first back
+        # # END of list, can't move right anymore
+        # print("END of list")
+        # print(f"helf item: {self._item}, positon: {self._position}")
+
+        # # move all the way to the left until you can't
+        # while self.can_move_left():
+        #     self.move_left()
+
+        # # START of list, can't move left anymore
+        # print("START of list")
+        # print(f"helf item: {self._item}, positon: {self._position}")
+
+        # # Held item should be smallest item in list, so put back at postition 0
+        # self.swap_item()
+        # print(f"helf item: {self._item}, positon: {self._position}")
+
+        print("--- END ---")
+        
+
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
     # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-    l = [5, 4, 3, 2, 1]
+    l = [2, 1, 3, 4, 5]
     robot = SortingRobot(l)
 
     robot.sort()
